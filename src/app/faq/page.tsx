76957253a421b58b949463b2f46110b1e5954d0b
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2025-08-30 17:11:07 -0400

    Spec out faq accordian

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,30 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/lib/components/Accordion";
 import React from "react";
 
 const FAQ = () => {
-  return <div>FAQ</div>;
+  return (
+    <section className="bg-background-green text-white text-body font-body">
+      <div className="container mx-auto p-6 lg:py-16">
+        <Accordion
+          type="single"
+          collapsible
+          className="bg-background-light text-foreground"
+        >
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </section>
+  );
 };
 
 export default FAQ;
